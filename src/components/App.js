387e7b911ba3c031bcebae0f19cc7ab6ac8339c1
Igor_Popovich <78104120+IgorPopovich@users.noreply.github.com>
2023-04-04 16:44:38 +0300

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -26,11 +26,10 @@ class App extends Component {
   }
 
   handleSubmitForm = (data) => {
-    for (let a of this.state.contacts) {
-      if (a.name.includes(data.name)) {
+    const { contacts } = this.state;
+    if (contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
         alert(`${data.name} is already in contacts`)
         return;
-      }
     }
       this.setState(prevState => ({
         contacts: [data, ...prevState.contacts],
